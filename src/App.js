5178326c6e4754dbff4618dd73affe566b4858ed
Md Samiul <simplysamiul@gmail.com>
2022-08-16 10:46:52 +0600

    crete navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className="navbar">
+        <ul className='menu'>
+          <li><a href="">Home</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Services</a>
+              <ul>
+                  <li><a href="">Development</a>
+                      <ul>
+                          <li><a href="">App Development</a></li>
+                          <li><a href="">Web Development</a></li>
+                          <li><a href="">Desktop App </a></li>
+                      </ul>
+                  </li>
+                  <li><a href="">Graphics Design</a>
+                      <ul>
+                          <li><a href="">Logo Design</a></li>
+                          <li><a href="">Banner Design</a></li>
+                          <li><a href="">Poster Design</a></li>
+                          <li><a href="">Template Design</a></li>
+                      </ul>
+                  </li>
+                  <li><a href="">Marketing</a>
+                      <ul>
+                          <li><a href="">Email Marketing</a></li>
+                          <li><a href="">Contet Marketing</a></li>
+                          <li><a href="">Online Marketing</a></li>
+                          <li><a href="">Affiliate Marketing</a></li>
+                      </ul>
+                  </li>
+              </ul>
+          </li>
+          <li><a href="">Others</a></li>
+        </ul>
+    </nav>
   );
 }
 
 export default App;
+
+
